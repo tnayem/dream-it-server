3e7071a2ae7d713f8caba7333720e6a854c01794
tnayem <tasdidnayem@gmail.com>
2022-10-25 13:58:35 +0600

    added data

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const app = express()
 app.use(cors())
 
 const port = process.env.Port || 5000
+
+const courseData = require('./data/courseData.json')
 app.get('/', (req, res) => {
   res.send('Dream it server is running')
+})
+app.get('/course-data', (req, res) => {
+  res.send(courseData)
 })
 
 app.listen(port, () => {
